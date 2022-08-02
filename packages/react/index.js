1cc73b45461036fc6081b20d44c4f1b55e66f01f
赵東澔 <zhaodonghao586@outlook.com>
2022-08-02 16:20:18 +0800

    feat: _

--- packages/react/index.js
+++ packages/react/index.js
@@ -9,10 +9,18 @@ module.exports = {
     },
   },
   rules: {
-    'jsx-quotes': [
-      'error',
-      'prefer-double',
-    ],
+    'jsx-quotes': ['error', 'prefer-double'],
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/jsx-wrap-multilines': ['error', {
+      return: 'parens-new-line',
+      assignment: 'parens-new-line',
+      arrow: 'parens-new-line',
+      condition: 'parens-new-line',
+      logical: 'parens-new-line',
+      prop: 'parens-new-line',
+    }],
+    '@typescript-eslint/no-unnecessary-type-constraint': 'off',
   },
 }
